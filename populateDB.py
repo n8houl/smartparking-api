a707b39c66df3550f8a2c8b5a15de53676882cc9
n8houl <n8houl@gmail.com>
2018-03-03 13:46:09 -0500

    Edited database population script in preparation for database migration

--- populateDB.py
+++ populateDB.py
@@ -7,24 +7,23 @@ class Pos:
 		self.lng = lng
 
 garages = {'A': 1623, 'B': 1259, 'C': 1852, 'D': 1241, 'H': 1284, 'I': 1231, 'Libra': 1007}
-geoInfo = {'A': Pos(28.599782, -81.205827), 'B': Pos(28.596813, -81.200324), 'C': Pos(28.602423, -81.195915), 'D': Pos(28.604942, -81.197189), 'H': Pos(28.604960, -81.201156), 'I': Pos(28.601125, -81.204892), 'Libra': Pos(28.595973, -81.196675), 'Test': Pos(28.595973, -81.196675)}
+geoInfo = {'A': Pos(28.599782, -81.205827), 'B': Pos(28.596813, -81.200324), 'C': Pos(28.602423, -81.195915), 'D': Pos(28.604942, -81.197189), 'H': Pos(28.604960, -81.201156), 'I': Pos(28.601125, -81.204892), 'Libra': Pos(28.595973, -81.196675), 'Test': Pos(0, 0)}
 
-"""
+ret = requests.get('http://localhost:3000/api/login/postable-user')
+token_data = json.loads(ret.text)
+token = token_data['token']
+
+header = {'Authorization': 'Bearer %s' % token, 'Content-type': 'application/json'}
+
 for key in garages.keys():
     for i in range(garages[key]):
         spotId = '%04d' % i
         url = 'http://localhost:3000/spots/%s' % key
         data = {"name" : "sensor_%s_garage%s" % (spotId, key.lower())}
         data_json = json.dumps(data)
-        headers = {'Content-type': 'application/json'}
 
-        ret = requests.post(url, data=data_json, headers=headers)
+        ret = requests.post(url, data=data_json, headers=header)
         print(ret.status_code)
-"""
-
-ret = requests.get('http://localhost:3000/api/login/postable-user')
-token_data = json.loads(ret.text)
-token = token_data['token']
 
 for key in geoInfo.keys():
 	p = geoInfo[key]
@@ -33,6 +32,6 @@ for key in geoInfo.keys():
 	name = "Garage"+key
 	data = {"garage": name, "latitude": lat, "longitude": lng}
 	data_json = json.dumps(data)
-	headers = {'Authorization': 'Bearer %s' % token, 'Content-type': 'application/json'}
 
-	ret = requests.post('http://localhost:3000/locations/%s' % key, data=data_json, headers=headers)
+	ret = requests.post('http://localhost:3000/locations/%s' % key, data=data_json, headers=header)
+	print(ret.status_code)
